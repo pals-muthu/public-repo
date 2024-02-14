33fb62c8c5ac52409d64b5bdf14a54da391df12c
pal-e5 <palaniappan.muthukaruppan@e5.ai>
2024-02-14 14:06:15 +0530

    minor improvements - added a table

--- github-users-library/src/pages/UserDetailPage.js
+++ github-users-library/src/pages/UserDetailPage.js
@@ -6,6 +6,13 @@ import { useLoaderData, defer, Await } from "react-router-dom"
 import { Grid } from "@mui/material"
 import classes from "./UserDetailPage.module.css"
 import Spinner from "../components/Spinner"
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 const UserDetailPage = () => {
   const { user } = useLoaderData()
@@ -17,69 +24,97 @@ const UserDetailPage = () => {
           <React.Suspense fallback={<Spinner />}>
             <Await resolve={user}>
               {(userInfo) => (
-                <Grid container>
-                  <Grid item xs={8}>
-                    <Grid>
-                      <p>Name: {userInfo.name}</p>
-                    </Grid>
-                    <Grid>
-                      <p>
-                        Username:{" "}
-                        <a href={userInfo.html_url} target="_blank">
-                          {userInfo.login}
-                        </a>
-                      </p>
-                    </Grid>
-                    <Grid>
+                <>
+                  <Grid container>
+                    <Grid item xs={8}>
                       <Grid>
-                        <p>Location: {userInfo.location}</p>
-                        <p>Available for hire: {userInfo.hireable ? "Yes" : "No"}</p>
+                        <p>Name: {userInfo.name}</p>
+                      </Grid>
+                      <Grid>
+                        <p>
+                          Username:{" "}
+                          <a href={userInfo.html_url} target="_blank">
+                            {userInfo.login}
+                          </a>
+                        </p>
+                      </Grid>
+                      <Grid>
+                        <Grid>
+                          <p>Location: {userInfo.location}</p>
+                          <p>Available for hire: {userInfo.hireable ? "Yes" : "No"}</p>
+                        </Grid>
+                      </Grid>
+                      <Grid>
+                        <p>
+                          Followers:{" "}
+                          <a href={userInfo.followers_url} target="_blank">
+                            {userInfo.followers}
+                          </a>
+                        </p>
+                      </Grid>
+                      <Grid>
+                        <p>
+                          Following:{" "}
+                          <a href={userInfo.following_url} target="_blank">
+                            {userInfo.following}
+                          </a>
+                        </p>
+                      </Grid>
+                      <Grid>
+                        <p>
+                          Blog:{" "}
+                          <a href={userInfo.blog || ""} target="_blank">
+                            {userInfo.blog || "NA"}
+                          </a>
+                        </p>
+                      </Grid>
+                      <Grid>
+                        <p>
+                          Repos:{" "}
+                          <a href={userInfo.repos_url} target="_blank">
+                            {userInfo.repoInfo.length}
+                          </a>
+                        </p>
                       </Grid>
                     </Grid>
-                    <Grid>
-                      <p>
-                        Followers:{" "}
-                        <a href={userInfo.followers_url} target="_blank">
-                          {userInfo.followers}
-                        </a>
-                      </p>
-                    </Grid>
-                    <Grid>
-                      <p>
-                        Following:{" "}
-                        <a href={userInfo.following_url} target="_blank">
-                          {userInfo.following}
-                        </a>
-                      </p>
-                    </Grid>
-                    <Grid>
-                      <p>
-                        Repos:{" "}
-                        <a href={userInfo.repos_url} target="_blank">
-                          {userInfo.repoInfo.length}
-                        </a>
-                      </p>
-                    </Grid>
-                    <Grid>
-                      <p>
-                        Blog:{" "}
-                        <a href={userInfo.blog || ""} target="_blank">
-                          {userInfo.blog || "NA"}
-                        </a>
-                      </p>
-                    </Grid>
-                    <Grid></Grid>
-                  </Grid>
-                  <Grid item xs={4}>
-                    <Grid>
-                      <img
-                        src={userInfo.avatar_url}
-                        height={"151px"}
-                        width={"151px"}
-                      />
+                    <Grid item xs={4}>
+                      <Grid>
+                        <img
+                          src={userInfo.avatar_url}
+                          height={"151px"}
+                          width={"151px"}
+                        />
+                      </Grid>
                     </Grid>
                   </Grid>
-                </Grid>
+                  <Grid className={classes.tableContainer}>
+                    {userInfo.repoInfo.length && <Grid>
+                      <TableContainer component={Paper}>
+                        <Table aria-label="simple table">
+                          <TableHead>
+                            <TableRow>
+                              <TableCell align="left">Repo Name</TableCell>
+                              <TableCell align="left">Forks</TableCell>
+                              <TableCell align="left">Watchers</TableCell>
+                            </TableRow>
+                          </TableHead>
+                          <TableBody>
+                            {userInfo.repoInfo.slice(0, 10).map((row) => (
+                              <TableRow
+                                key={row.full_name}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                              >
+                                <TableCell align="left"><a href={row.html_url} target="_blank">{row.full_name}</a></TableCell>
+                                <TableCell align="left">{row.forks}</TableCell>
+                                <TableCell align="left">{row.watchers}</TableCell>
+                              </TableRow>
+                            ))}
+                          </TableBody>
+                        </Table>
+                      </TableContainer>
+                    </Grid>}
+                  </Grid>
+                </>
               )}
             </Await>
           </React.Suspense>
